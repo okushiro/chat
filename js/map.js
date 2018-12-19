0674e4e4a976f8d1ae38a1e9d9e72e8c75c1004d
Kentaro Okushiro <ken09460308@gmail.com>
2018-12-19 22:25:09 +0900

    地図の追加

---
+++ js/map.js
@@ -0,0 +1,66 @@
+
+
+function makeMap(lat, lng) {    
+    var canvas = document.getElementById('map'); // 地図を表示する要素を取得
+
+    var latlng = new google.maps.LatLng(lat, lng);  // 中心の位置（緯度、経度）
+
+    var mapOptions = {  // マップのオプション
+        zoom: 10,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false,
+        overviewMapControl: false,
+        zoomControl: true,
+        draggable: true
+    };
+
+    var map = new google.maps.Map(canvas, mapOptions); //作成
+
+    var marker = new google.maps.Marker({
+        position: latlng, // マーカーを立てる位置を指定
+        map: map, // マーカーを立てる地図を指定
+        animation: google.maps.Animation.DROP
+    });
+
+    return [map,marker];
+}
+
+function mapsInit(position){
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    newPostRef.push({
+        mapUser: localStorage.getItem('user'),
+        lat: lat,
+        lng: lng
+    });
+}
+
+function mapsError(){
+    newPostRef.push({
+        mapUser: localStorage.getItem('user'),
+        lat: 35.667224,
+        lng: 139.713833
+    });
+}
+
+const set = {
+    enableHighAccuracy: true,
+    maximumAge: 20000,
+    timeout: 10000
+}
+
+
+//ページのロードが完了したら地図を読み込む
+window.onload = function(){
+    navigator.geolocation.getCurrentPosition(mapsInit,mapsError,set);
+    newPostRef.on('child_added', function(data){
+        let v = data.val();
+        let k = data.key;
+
+        if(v.mapUser){
+            makeMap(v.lat, v.lng);
+        }   
+    });
+     
+};
